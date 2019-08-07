5cfaee3681fb13edc6884e9159cd7d5a47f8eaef
Bethan Perkins <bethan.perkins@assimila.eu>
2019-08-07 16:25:20 +0100

    Updating regions code

--- DQTools/regions/__init__.py
+++ DQTools/regions/__init__.py
@@ -5,10 +5,18 @@ from collections import namedtuple
 
 def get_bounds(region):
     """
-    Return the bounds for a region
-    :param region:
+    Parse the regions.yaml config file and extract bounds information for
+    reading data.
+
+    NOTE: It is not advised to try and write data for a region, unless they
+    are an exact match to a tile. If the dataextents don't match a tile in
+    the DataCube they will be kicked out with an error. Use tiles for data
+    that needs to be written.
+
+    :param region: name of a region defined in the bounds config file.
     :return:
     """
+
     # Define the directory that this file is sitting in
     path = op.abspath(op.dirname(__file__))
 
@@ -23,17 +31,19 @@ def get_bounds(region):
         raise KeyError(f'Region {region} does not exist in regions.yaml')
 
     # Set up structure of named tuple to hold the information
-    Bounds = namedtuple('Bounds','north south east west')
+    Bounds = namedtuple('Bounds', 'north south east west')
 
     # Add bounds to named tuple instance
     region_bounds = Bounds(**bounds_dict)
 
     return region_bounds
 
-def add_region(region):
-    #Todo: a method where a user can programatically add regions.
-    pass
 
-def show_regions():
-    #Todo: a method where a user can programatically view all available regions.
-    pass
+# Todo: This part of the code could be extended to do some or all of the
+#       following:
+#
+#       1. Allow a user to pass a latitude/longitude point
+#       2. Allow an interface to graphical area-select tools
+#       3. Integrate with geo-locations library to allow a user to enter a city
+#           or country name instead of lat/lon information
+#
