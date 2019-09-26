7aef62dc27c2343f4ea5d4cda8413742f2ec14c8
taylorday <taylor.day@assimila.eu>
2019-09-26 09:41:53 +0100

    ensuring dataset can be initialised without a tile set

--- DQTools/dataset.py
+++ DQTools/dataset.py
@@ -161,7 +161,7 @@ Data:
 
         # Filter by selected tile so that mosaicking does not impact return
         # If >1 tilename specified
-        if len(list(set(all_meta.tilename))) > 1:
+        if len(list(set(all_meta.tilename))) > 1 and self.tile:
             all_meta = all_meta.loc[all_meta['tilename'] == self.tile]
 
         # Extract the last timestep
